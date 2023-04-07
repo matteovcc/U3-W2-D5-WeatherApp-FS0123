1aea0ba6179431dbc1d0273708334f4882053d7d
matteovcc <matteovcc99@gmail.com>
2023-04-07 14:14:30 +0200

    edit

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 // import City from "./City";
 
@@ -23,12 +23,34 @@ const MainSearch = () => {
 
         setCities(data);
       } else {
-        alert("Error fetching results");
+        alert("Error fetching cities");
       }
     } catch (error) {
       console.log(error);
     }
   };
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${cities[0].lat}&lon=${cities[0].lon}&appid=34c6980fa47e80410952d0cf568636d2`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setWeather(data);
+      } else {
+        alert("error fetching weather");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+    // handleSubmit();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cities]);
 
   return (
     <Container>
@@ -47,13 +69,25 @@ const MainSearch = () => {
           </Form>
         </Col>
         <Col xs={10} className="mx-auto mb-5">
-          {cities.length > 0 && (
-            <div className="border border-warning rounded px-2 mt-2 d-flex align-items-center div-city">
-              <p className="text-white fw-bold my-1">
-                {cities[0].name},{cities[0].country}
-              </p>
-            </div>
-          )}
+          <div className="border border-warning rounded px-2 mt-2 d-flex align-items-center div-city">
+            {cities.length > 0 && (
+              <>
+                <p className="text-white fw-bold my-1">
+                  {cities[0].name},{cities[0].state}
+                  {cities[0].country} |
+                </p>
+                <p className="text-white  my-1 ms-auto">
+                  La temperatura è:
+                  {weather.main.temp},umidità {weather.main.humidity},
+                  pressione:
+                  {weather.main.pressure}
+                </p>
+              </>
+            )}
+            {/* {weather.length > 0 && (
+              <p className="text-white">{weather.main.temp}djdjdj</p>
+            )} */}
+          </div>
         </Col>
       </Row>
     </Container>
